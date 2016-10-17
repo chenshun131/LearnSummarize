f402145d198ca6c4c875e9d4795c0b7da41f564b
chenshun131 <1539831174@qq.com>
2016-10-17 17:41:13 +0800

    提交 显示网络图片信息

--- iOS/LearnSummarize/LearnSummarize/AppDelegate.m
+++ iOS/LearnSummarize/LearnSummarize/AppDelegate.m
@@ -19,7 +19,12 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
     [self initNetwork];
+    
+    NSString *resPath = [[NSBundle mainBundle] resourcePath];
     
     return YES;
 }
